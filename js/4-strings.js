0f83757aae0e383256b8231a4c5a49dd9873c228
Bryn-13 <p.palomino95@gmail.com>
2021-12-13 00:33:51 -0300

    intro-strings.

--- js/4-strings.js
+++ js/4-strings.js
@@ -1,3 +1,9 @@
-let myVar = 1;
+const producto = "Monitor de 20\"";
+const producto2 = String("Monitor de 30 Pulgadas"); // Casi no se usa
+const producto3 = new String("Monitor de 50 Pulgadas") // "new" lo convierte en objeto. Casi no se usa
 
-console.log(myVar)
+console.log( producto)
+console.log(producto2)
+console.log(producto3)
+
+
