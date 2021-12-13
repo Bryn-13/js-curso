3cd5accdb1cc734113fbc28974518caba2c1e0e1
Bryn-13 <p.palomino95@gmail.com>
2021-12-13 01:17:16 -0300

    operadores.

--- js/5-numeros.js
+++ js/5-numeros.js
@@ -1,14 +1,30 @@
 // Números
 
-const numero1 = 100;
-const numero2 = 200;
-const numero3 = 1654;
-const numero4 = 18;
-const numero5 = -5;
+// const numero1 = 100;
+// const numero2 = 200;
+// const numero3 = 1654;
+// const numero4 = 18;
+// const numero5 = -5;
 
-console.log(numero1 + numero2)
-console.log(numero2 - numero1)
-console.log(numero2 * numero1)
-console.log(numero2 / numero1)
-console.log(numero3 % numero4)
+// console.log(numero1 + numero2)
+// console.log(numero2 - numero1)
+// console.log(numero2 * numero1)
+// console.log(numero2 / numero1)
+// console.log(numero3 % numero4)
 
+// -- OBJETO MATH --
+
+let resultado;
+
+resultado = Math.pi;
+resultado = Math.round (2.5);
+resultado = Math.ceil(4.3); // Redondea siempre hacia arriba
+resultado = Math.floor(4.9); // Redondea siempre hacia abajo
+resultado = Math.sqrt(144); // Raiz cuadrada
+resultado = Math.abs(-200); // Convierte negativo en positivo
+resultado = Math.min(3, 5, 8, 1, 2, 10);
+resultado = Math.max(3, 5, 8, 1, 2, 10);
+resultado = Math.random(); // Número aleatorio enter 0 y 1
+resultado = Math.floor(Math.random() * 30);
+
+console.log(resultado)
