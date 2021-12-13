883f9e0bbea8d33666109f14e7909d468cd5f07f
Bryn-13 <p.palomino95@gmail.com>
2021-12-13 00:42:49 -0300

    strings.

--- js/4-strings.js
+++ js/4-strings.js
@@ -1,9 +1,26 @@
-const producto = "Monitor de 20\"";
-const producto2 = String("Monitor de 30 Pulgadas"); // Casi no se usa
-const producto3 = new String("Monitor de 50 Pulgadas") // "new" lo convierte en objeto. Casi no se usa
+// const producto = "Monitor de 20\"";
+// const producto2 = String("Monitor de 30 Pulgadas"); // Casi no se usa
+// const producto3 = new String("Monitor de 50 Pulgadas") // "new" lo convierte en objeto. Casi no se usa
 
-console.log( producto)
-console.log(producto2)
-console.log(producto3)
+// console.log( producto)
+// console.log(producto2)
+// console.log(producto3)
 
 
+// -- MÉTODOS --
+
+const tweet = "Aprendiendo JavaScript con el curso de Desarrollo Web";
+const producto2 = "Monitor HD"
+
+console.log(tweet.length); // length es para la extensión
+console.log(producto2);
+
+// IndexOf (retorna posición)
+
+console.log(tweet.indexOf('JavaScript')) // indexOf es para encontrar un elemento en un string
+console.log(tweet.indexOf('Tablet'))
+
+// Includes (retorna true o false)
+
+console.log(tweet.includes('JavaScript')) // indexOf es para encontrar un elemento en un string
+console.log(tweet.includes('Tablet'))
