2bd672c94abed74c2e7ac5d563d560181f750f8a
Bryn-13 <p.palomino95@gmail.com>
2021-12-13 01:29:36 -0300

    orden-operaciones.

--- js/5-numeros.js
+++ js/5-numeros.js
@@ -14,17 +14,38 @@
 
 // -- OBJETO MATH --
 
+// let resultado;
+
+// resultado = Math.pi;
+// resultado = Math.round (2.5);
+// resultado = Math.ceil(4.3); // Redondea siempre hacia arriba
+// resultado = Math.floor(4.9); // Redondea siempre hacia abajo
+// resultado = Math.sqrt(144); // Raiz cuadrada
+// resultado = Math.abs(-200); // Convierte negativo en positivo
+// resultado = Math.min(3, 5, 8, 1, 2, 10);
+// resultado = Math.max(3, 5, 8, 1, 2, 10);
+// resultado = Math.random(); // Número aleatorio enter 0 y 1
+// resultado = Math.floor(Math.random() * 30);
+
+// console.log(resultado)
+
+// -- OPERADORES --
+
 let resultado;
 
-resultado = Math.pi;
-resultado = Math.round (2.5);
-resultado = Math.ceil(4.3); // Redondea siempre hacia arriba
-resultado = Math.floor(4.9); // Redondea siempre hacia abajo
-resultado = Math.sqrt(144); // Raiz cuadrada
-resultado = Math.abs(-200); // Convierte negativo en positivo
-resultado = Math.min(3, 5, 8, 1, 2, 10);
-resultado = Math.max(3, 5, 8, 1, 2, 10);
-resultado = Math.random(); // Número aleatorio enter 0 y 1
-resultado = Math.floor(Math.random() * 30);
+resultado = (20 + 30) * 2;
+
+resultado = (100 + 200 + 300) * 1.16
 
 console.log(resultado)
+
+// Incrementos 
+
+let puntaje = 10;
+
+
+console.log(++puntaje)
+
+puntaje += 10;
+
+console.log(puntaje)
