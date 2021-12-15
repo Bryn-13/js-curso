2e1d0d96e22891ba4f001f2c947e09fb5b59bedf
Bryn-13 <p.palomino95@gmail.com>
2021-12-15 01:13:38 -0300

    more-arrays.

--- js/9-arrays.js
+++ js/9-arrays.js
@@ -4,7 +4,7 @@ const numeros = [10,20,30,40,50];
 
 // console.table(numeros)
 
-const meses = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo");
+// const meses = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo");
 // console.table(meses);
 
 // const arreglo = ["Hola", 10 , true, "si", null, {nombre: "Paulina", apellido: "Palomino"}, [1,2,3]]
@@ -30,11 +30,11 @@ const meses = new Array("Enero", "Febrero", "Marzo", "Abril", "Mayo");
 
 // numeros[5] = 60; // No se utiliza mucho, hay que saber la extensión del arreglo porque puede modificar otros elementos si se ingresa una posición ya ocupada
 
-numeros.push(60); // Agrega al final. Actualmente se recomienda no modificar los arreglos originales
+// numeros.push(60); // Agrega al final. Actualmente se recomienda no modificar los arreglos originales
 
-numeros.unshift(-10,-20,-30) // Agrega al inicio
+// numeros.unshift(-10,-20,-30) // Agrega al inicio
 
-console.table(numeros);
+// console.table(numeros);
 
 // ** -- QUITAR ELEMENTOS DE ARREGLO -- **
 
@@ -46,7 +46,69 @@ console.table(numeros);
 
 // ** -- RESTO OPERATOR O SPREAD OPERATOR -- **
 
-const nuevoArreglo = [...meses, "Junio"]; // <-- UTILIZAR ESTE MÉTODO. No se modifica el arreglo original
+// const nuevoArreglo = [...meses, "Junio"]; // <-- UTILIZAR ESTE MÉTODO. No se modifica el arreglo original
 
-console.table(nuevoArreglo)
+// console.table(nuevoArreglo)
 
+
+// ** -- MÁS MÉTODOS -- **
+
+const meses = ["Enero", "Febrero", "Marzo", "Abril", "Mayo"];
+
+
+const carrito = [
+    {nombre: "Monitor HD", precio: 500},
+    {nombre: "Televisor 50\"", precio: 700},
+    {nombre: "Tablet", precio: 300},
+    {nombre: "Audífonos", precio: 200},
+    {nombre: "Teclado", precio: 50},
+    {nombre: "Celular", precio: 500},
+    {nombre: "Bocinas", precio: 300},
+    {nombre: "Laptop", precio: 800},
+];
+
+// forEach
+
+meses.forEach(function(mes) {
+    if(mes = "Marzo") {
+        console.log("Marzo sí existe")
+    }
+    else {
+        console.log("Marzo no existe")
+    }
+}
+);
+
+// Includes
+
+let resultado = meses.includes("Diciembre");
+
+
+// Some ideal para arreglo de objetos
+
+resultado = carrito.some(function(producto) {
+    return producto.nombre === "Celular "
+})
+
+console.log(resultado);
+
+// Reduce
+
+resultado = carrito.reduce(function(total, producto) {
+    return total + producto.precio
+}, 0)
+
+console.log(resultado);
+
+// Filter
+
+resultado = carrito.filter(function(producto) {
+    return producto.precio > 400
+})
+
+resultado = carrito.filter(function(producto) {
+    return producto.nombre === "Celular"
+})
+
+
+console.log(resultado);
