68b528f969cfe2668a3bd7ca9729accdff2042dd
Bryn-13 <p.palomino95@gmail.com>
2021-12-16 21:10:25 -0300

    funciones.

--- js/10-funciones.js
+++ js/10-funciones.js
@@ -22,3 +22,19 @@ sumar2();
 (function() {
    console.log("Esto es una función")
 })(); // Previene que no se puedan leer de otros archivos
+
+
+// -- ** HOISTING ** --
+
+sumar();
+function sumar() {
+    console.log(10 + 10);
+} // Funciona, el hoisting primero reconoce la variable
+// y en la segunda lectura la llama
+
+
+sumar2();
+const sumar2 = function() {
+    console.log(3 + 3);
+} // No funciona. El hoisting toma la función como variable, 
+// y en la segunda lectura no encuentra la función a la que llamar
