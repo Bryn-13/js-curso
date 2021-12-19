c4909e5f8290583a3b8524b51897f2c67e4360de
Bryn-13 <p.palomino95@gmail.com>
2021-12-19 17:27:11 -0300

    métodos.

---
+++ js/11-métodos.js
@@ -0,0 +1,26 @@
+// Método de propiedad
+
+// const reproductor = {
+//     reproducir: function() {
+//         console.log(`Reproduciendo canción con el ID: ${3840}`)
+//     },
+//     pausar: function() {
+//         console.log("Pausando...")
+//     },
+//     crearPlaylist: function(nombre) {
+//         console.log(`Creando la playlist: ${nombre}`)
+//     },
+//     reproducirPlaylist: function(nombre) {
+//         console.log(`Reproduciendo playlist: ${nombre}`)
+//     },
+// }
+
+// reproductor.borrarCanción = function(id) { // Añadiendo propiedad fuera del objeto
+//     console.log(`Eliminando la canción: ${id}`)
+// }
+
+// reproductor.reproducir(3840);
+// reproductor.pausar();
+// reproductor.borrarCanción(20);
+// reproductor.crearPlaylist("Heavy Metal");
+// reproductor.reproducirPlaylist("Heavy Metal");
